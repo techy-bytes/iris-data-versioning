dbb33d2fdb504f44b69245ac7473595ee49d73ad
jl <129856302@users.noreply.github.com>
2025-06-01 01:02:43 +0530

    update location

--- double_iris.py
+++ double_iris.py
@@ -3,7 +3,7 @@ import pandas as pd
 def double_iris():
     df = pd.read_csv('data/iris.csv')
     df_doubled = pd.concat([df, df], ignore_index=True)
-    df_doubled.to_csv('iris.csv', index=False)
+    df_doubled.to_csv('data/iris.csv', index=False)
 
 if __name__ == "__main__":
     double_iris()
